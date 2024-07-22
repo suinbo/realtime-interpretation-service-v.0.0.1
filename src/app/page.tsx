492fc58131a0c54cf9044c87b8b9f0590df67692
suinbo <suin9610@gmail.com>
2024-07-22 17:09:51 +0900

    Feat: 회원가입 구현 중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,25 @@ import Image from "next/image"
 import { useRouter } from "next/navigation"
 import { Button, Input } from "@components/form"
 import "./style.scss"
+import { supabase } from "@utils/superbase"
 
 /**
  * 로그인 화면
  */
 const Login = () => {
-    const router = useRouter()
+    // const router = useRouter()
+
+    const handleSignUp = async () => {
+        //e.preventDefault();
+        const { user, error } = await supabase.auth.signUp({
+            email: "suin9610@gmail.com",
+            password: "123456*",
+        })
+
+        if (error) {
+            //   setMessage(error.message);
+        }
+    }
 
     return (
         <div className="login">
@@ -19,8 +32,8 @@ const Login = () => {
                         <Image src="/images/logo.png" alt="로고 이미지" width={166} height={114} />
                     </div>
                     <div className="logo__title__text">
-                        <h3>실시간 동시통역 서비스</h3>
-                        <h1>LOGIN</h1>
+                        <p className="typo t30 w500">실시간 동시통역 서비스</p>
+                        <p className="typo t50 w600">LOGIN</p>
                     </div>
                 </div>
                 <div className="login__form">
@@ -30,7 +43,14 @@ const Login = () => {
                     <div className="login__form__item">
                         <Input placeholder="Password" type="password" />
                     </div>
-                    <Button classname="typo t20 w500" text="Sign In" onClick={() => router.push("/setting")} />
+                    <Button
+                        classname="typo t20 w500"
+                        text="Sign In"
+                        onClick={() => {
+                            // router.push("/setting")
+                        }}
+                    />
+                    <Button classname="typo t20 w500" text="Sign Up" onClick={handleSignUp} />
                 </div>
             </div>
         </div>
