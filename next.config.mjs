9894f6a897bf66ce536aa4fac77bb892f03153c1
suinbo <suin9610@gmail.com>
2024-07-23 15:48:07 +0900

    Chore: @assets 경로 추가

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,7 @@ const nextConfig = {
             ...config.resolve.alias,
             "@app": path.resolve(__dirname, "src/app"),
             "@atoms": path.resolve(__dirname, "src/atoms"),
+            "@assets": path.resolve(__dirname, "src/assets"),
             // "@images": path.resolve(__dirname, "src/assets/images"),
             "@components": path.resolve(__dirname, "src/components"),
             "@hooks": path.resolve(__dirname, "src/hooks"),
